ce7cd97e189fc4c599191c304bbf5548dc538226
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-08-27 21:01:46 +0900

    Initial Commit

---
+++ FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by JONGHUN PARK on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
